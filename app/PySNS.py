af012473d651c213bec550c51998b679c4cab4f3
Racer1984 <31016332+Racer1984@users.noreply.github.com>
2020-01-25 15:56:07 +0300

    Added temporary credentials from IAM role
    Temporary creds replaced hard-coded ones.

--- app/PySNS.py
+++ app/PySNS.py
@@ -1,9 +1,12 @@
 import boto3
 import configparser
+import requests
 
-def create_client(profile_name):
+def create_client():
 
-    session = boto3.Session(profile_name = profile_name, region_name = 'us-east-2')
+    r = requests.get('http://169.254.169.254/latest/meta-data/iam/security-credentials/AllowSNS_AllowSSM')
+
+    session = boto3.Session(aws_access_key_id = r.json()['AccessKeyId'], aws_secret_access_key = r.json()['SecretAccessKey'], aws_session_token = r.json()['Token'], region_name = 'us-east-2')
     sns_client = session.client('sns')
 
     return sns_client
--- app/views.py
+++ app/views.py
@@ -12,7 +12,7 @@ def index():
     form = SendForm()
     if form.validate_on_submit():
         flash('You just sent "' + form.msg.data + '" to AWS SNS')
-        sns = create_client('sns-tester')
+        sns = create_client()
         response = sns.publish(
             TopicArn = 'arn:aws:sns:us-east-2:591559526277:test-sns',
             Message = form.msg.data
