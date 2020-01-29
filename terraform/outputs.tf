25242f96d8c0874fa92ae27ae87234394d1b12b4
Racer1984 <31016332+Racer1984@users.noreply.github.com>
2020-01-29 20:30:57 +0300

    Update outputs.tf
    Added app URL for output

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,6 @@ output "NAT_GW_IP" {
   value = aws_eip.eip.public_ip
 }
 
-output "APPLICATION_DNS_NAME" {
-  value = aws_lb.sns-app-alb.dns_name
+output "APPLICATION_URL" {
+  value = format("http://%s", aws_lb.sns-app-alb.dns_name)
 }
